fe57e7bab52b8a39c2e9c955a388e1968719c2c5
Kouji Matsui <k@kekyo.net>
2022-03-20 23:22:46 +0900

    Refactored.

---
+++ FlashCap/CaptureDeviceExtension.cs
@@ -0,0 +1,88 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// FlashCap - Independent camera capture library.
+// Copyright (c) Kouji Matsui (@kozy_kekyo, @kekyo@mastodon.cloud)
+//
+// Licensed under Apache-v2: https://opensource.org/licenses/Apache-2.0
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlashCap
+{
+    public static class CaptureDeviceExtension
+    {
+        public static PixelBuffer CaptureOneShot(
+            this ICaptureDevice device)
+        {
+            using var waiter = new ManualResetEventSlim(false);
+            var buffer = new PixelBuffer();
+            
+            void FrameArrived(object sender, FrameArrivedEventArgs e)
+            {
+                device.Capture(e, buffer);
+                device.FrameArrived -= FrameArrived!;
+                waiter.Set();
+            }
+
+            var isRunning = device.IsRunning;
+            device.FrameArrived += FrameArrived!;
+            try
+            {
+                if (!isRunning)
+                {
+                    device.Start();
+                }
+                waiter.Wait();
+            }
+            finally
+            {
+                if (!isRunning)
+                {
+                    device.Stop();
+                }
+                device.FrameArrived -= FrameArrived!;
+            }
+
+            return buffer;
+        }
+
+#if !(NET20 || NET35 || NET40)
+        public static async Task<PixelBuffer> CaptureOneShotAsync(
+            this ICaptureDevice device)
+        {
+            var tcs = new TaskCompletionSource<PixelBuffer>();
+            var buffer = new PixelBuffer();
+            
+            void FrameArrived(object sender, FrameArrivedEventArgs e)
+            {
+                device.Capture(e, buffer);
+                device.FrameArrived -= FrameArrived!;
+                tcs.SetResult(buffer);
+            }
+
+            var isRunning = device.IsRunning;
+            device.FrameArrived += FrameArrived!;
+            try
+            {
+                if (!isRunning)
+                {
+                    device.Start();
+                }
+
+                return await tcs.Task.ConfigureAwait(false);
+            }
+            finally
+            {
+                if (!isRunning)
+                {
+                    device.Stop();
+                }
+                device.FrameArrived -= FrameArrived!;
+            }
+        }
+#endif
+    }
+}
