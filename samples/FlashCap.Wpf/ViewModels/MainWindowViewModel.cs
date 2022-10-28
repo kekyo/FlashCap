512142faa1c4fdc4a24ee928ff373aff2b2bfe60
Kouji Matsui <k@kekyo.net>
2022-10-28 18:52:13 +0900

    Added more F# bindings.

--- samples/FlashCap.Wpf/ViewModels/MainWindowViewModel.cs
+++ samples/FlashCap.Wpf/ViewModels/MainWindowViewModel.cs
@@ -67,7 +67,7 @@ namespace FlashCap.Wpf.ViewModels
                         this.OnPixelBufferArrivedAsync);
 
                     // Start capturing.
-                    this.captureDevice.OnStart();
+                    this.captureDevice.Start();
                 }
                 else
                 {
