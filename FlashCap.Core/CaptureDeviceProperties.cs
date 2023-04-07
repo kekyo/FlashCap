6e4b64c3da7276f8cc8a8e1755a0ebdf9218bc36
ldlac <louis-david.lacerte@can-explore.com>
2023-04-06 22:57:05 -0400

    testing with brightness

---
+++ FlashCap.Core/CaptureDeviceProperties.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace FlashCap
+{
+    public enum VideoProcessingAmplifierProperty
+    {
+        Brightness,
+        Contrast,
+        Hue,
+        Saturation,
+        Sharpness,
+        Gamma,
+        ColorEnable,
+        WhiteBalance,
+        BacklightCompensation,
+        Gain
+    }
+
+    public sealed class CaptureDeviceProperties : Dictionary<VideoProcessingAmplifierProperty, CaptureDeviceProperty>
+    {
+    }
+
+    public abstract class CaptureDeviceProperty
+    {
+        public VideoProcessingAmplifierProperty Property { get; private set; }
+        public int Min { get; private set; }
+        public int Max { get; private set; }
+        public int Step { get; private set; }
+
+        protected CaptureDeviceProperty(VideoProcessingAmplifierProperty property, int min, int max, int step)
+        {
+            Property = property;
+            Min = min;
+            Max = max;
+            Step = step;
+        }
+
+        internal bool IsPropertyValueValid(object? obj)
+        {
+            var value = obj as int?;
+
+            return value != null && value <= Max && value >= Min && value % Step == 0;
+        }
+    }
+}
---
+++ FlashCap.Core/Devices/DirectShowProperty.cs
@@ -0,0 +1,14 @@
+﻿using FlashCap.Internal;
+
+namespace FlashCap.Devices
+{
+    public sealed class DirectShowProperty : CaptureDeviceProperty
+    {
+        public DirectShowProperty(VideoProcessingAmplifierProperty property, int min, int max, int step) : base(property, min, max, step) { }
+
+        internal static NativeMethods_DirectShow.VideoProcAmpProperty FromVideoProcessingAmplifierProperty(VideoProcessingAmplifierProperty property)
+        {
+            return (NativeMethods_DirectShow.VideoProcAmpProperty)(int)property;
+        }
+    }
+}
