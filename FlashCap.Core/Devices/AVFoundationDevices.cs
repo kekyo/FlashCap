0b855e28537e2967ca61577b9051e9598564d52f
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-06-23 21:12:01 +0300

    Device discovery and pixel format

--- FlashCap.Core/Devices/AVFoundationDevices.cs
+++ FlashCap.Core/Devices/AVFoundationDevices.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using FlashCap.Internal;
 using FlashCap.Utilities;
 using static FlashCap.Internal.NativeMethods_AVFoundation.LibAVFoundation;
+using static FlashCap.Internal.NativeMethods_AVFoundation.LibCoreVideo;
 
 namespace FlashCap.Devices;
 
@@ -19,15 +20,32 @@ public sealed class AVFoundationDevices : CaptureDevices
 {
     protected override IEnumerable<CaptureDeviceDescriptor> OnEnumerateDescriptors()
     {
-        using var discovery = AVCaptureDeviceDiscoverySession.DiscoverySessionWithVideoDevices();;
+        using var discovery = AVCaptureDeviceDiscoverySession.DiscoverySessionWithVideoDevices();
         return discovery.Devices
             .Select(static device =>
-                new AVFoundationDeviceDescriptor(
+            {
+                using var deviceInput = new AVCaptureDeviceInput(device);
+                using var deviceOutput = new AVCaptureVideoDataOutput();
+
+                using var session = new AVCaptureSession();
+
+                session.AddInput(deviceInput);
+                session.AddOutput(deviceOutput);
+
+                var supportedPixelFormats = NativeMethods_AVFoundation.PixelFormatMap;
+                var availablePixelFormats = (
+                    from available in deviceOutput.AvailableVideoCVPixelFormatTypes
+                    join supported in supportedPixelFormats on available equals supported.Value into temp
+                    from supported in temp.Select(format => format.Key).DefaultIfEmpty(PixelFormats.Unknown)
+                    select new KeyValuePair<PixelFormats, string>(supported, GetFourCCName(available)))
+                    .ToArray();
+ 
+                return new AVFoundationDeviceDescriptor(
                     device.UniqueID,
                     device.ModelID,
                     device.LocalizedName,
                     device.Formats
-                        .SelectMany(static format =>
+                        .SelectMany(format =>
                         {
                             var description = format.FormatDescription;
                             var dimensions = description.Dimensions;
@@ -42,8 +60,11 @@ public sealed class AVFoundationDevices : CaptureDevices
                             return NativeMethods.DefactoStandardFramesPerSecond
                                 .Where(fps => fps >= minFps && fps <= maxFps)
                                 .OrderByDescending(fps => fps)
-                                .Select(fps => new VideoCharacteristics(default, dimensions.Width, dimensions.Height, fps));
+                                .SelectMany(fps =>
+                                    availablePixelFormats.Select(
+                                        format =>new VideoCharacteristics(format.Key, dimensions.Width, dimensions.Height, fps, format.Value, isDiscrete: true, format.Value)));
                         })
-                        .ToArray()));
+                        .ToArray());
+            });
     }
 }
