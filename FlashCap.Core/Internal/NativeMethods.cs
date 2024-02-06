b64deee429cd3baa660513bae8019845ae87106e
Kevin Smith <kevin@muny.us>
2024-02-05 20:16:09 -0500

    Add NV12 support (V4L2 needs work)

--- FlashCap.Core/Internal/NativeMethods.cs
+++ FlashCap.Core/Internal/NativeMethods.cs
@@ -199,7 +199,8 @@ internal static class NativeMethods
         YUYV = 0x56595559,   // FOURCC
         UYVY = 0x59565955,   // FOURCC
         MJPG = 0x47504A4D,   // FOURCC
-        HDYC = 0x43594448    // FOURCC (BlackMagic input (UYVY))
+        HDYC = 0x43594448,   // FOURCC (BlackMagic input (UYVY))
+        NV12 = 0x3231564E,   // FOURCC
     }
 
     private static int CalculateClrUsed(
@@ -506,6 +507,7 @@ internal static class NativeMethods
             Compression.YUYV => PixelFormats.YUYV,
             Compression.YUY2 => PixelFormats.YUYV,
             Compression.HDYC => PixelFormats.YUYV,
+            Compression.NV12 => PixelFormats.NV12,
             _ => PixelFormats.Unknown,
         };
 
@@ -578,6 +580,10 @@ internal static class NativeMethods
             case PixelFormats.YUYV:
                 compression = Compression.YUYV;
                 bitCount = 16;
+                return true;
+            case PixelFormats.NV12:
+                compression = Compression.NV12;
+                bitCount = 12;
                 return true;
             default:
                 compression = default;
