a781a6e0d165e14011d40f781ab5c5d13dd7f64b
Felipe Quintella <felipe@quintella.com>
2025-03-26 09:01:26 -0300

    Tests #3

--- FlashCap.Core/Devices/AVFoundationDevice.cs
+++ FlashCap.Core/Devices/AVFoundationDevice.cs
@@ -109,7 +109,7 @@ public sealed class AVFoundationDevice : CaptureDevice
                 dimensions.Height == characteristics.Height)
             ?? throw new InvalidOperationException(
                 $"FlashCap: Couldn't set video format: UniqueID={this.uniqueID}");
-        this.device.UnlockForConfiguration();*/
+        this.device.UnlockForConfiguration();
 
         var frameDuration = CMTimeMake(
             characteristics.FramesPerSecond.Denominator,
@@ -117,6 +117,7 @@ public sealed class AVFoundationDevice : CaptureDevice
         
         device.ActiveVideoMinFrameDuration = frameDuration;
         device.ActiveVideoMaxFrameDuration = frameDuration;
+        */
 
         this.deviceInput = new AVCaptureDeviceInput(device);
         
