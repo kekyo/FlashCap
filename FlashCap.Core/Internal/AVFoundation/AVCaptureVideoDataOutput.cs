b0a14b91d0e7686757b1c3414f6ba796d0c7dcc6
Felipe Quintella <felipe@quintella.com>
2025-05-27 08:11:51 -0300

    fix dispose

--- FlashCap.Core/Internal/AVFoundation/AVCaptureVideoDataOutput.cs
+++ FlashCap.Core/Internal/AVFoundation/AVCaptureVideoDataOutput.cs
@@ -187,7 +187,7 @@ partial class LibAVFoundation
                 {
                     if (callbackDelegate != null)
                     {
-                        Marshal.FreeHGlobal(Marshal.GetFunctionPointerForDelegate(callbackDelegate));
+                        //Marshal.FreeHGlobal(Marshal.GetFunctionPointerForDelegate(callbackDelegate));
                         callbackDelegate = null;
                     }
                 }
